f1848fb8b92ab09a9f8b76e485b3631aede8a4fa
Constantine Zubarev <kv.zubarev@gmail.com>
2022-05-18 21:56:53 +0600

    style: add sanitize.css

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { createRoot } from 'react-dom/client'
 import Demo from './__demo__'
 import reportWebVitals from './reportWebVitals'
 
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/typography.css'
 import './index.css'
 
 const app = (
